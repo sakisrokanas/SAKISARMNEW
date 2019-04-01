15c269b7a1c9ac97960b391b0028786d4ab6d8e6
sakisrokanas <sakis.rokanas@microsoft.com>
2019-04-01 18:32:07 +0300

    Update hanastudio.ps1

--- hanastudio.ps1
+++ hanastudio.ps1
@@ -12,8 +12,8 @@ param (
     #7zUri = "http://www.7-zip.org/a/7z1701-x64.msi"
     $sapcardest = "C:\SapBits\SAP_HANA_STUDIO\sapcar.exe"
     $hanastudiodest = "C:\SapBits\SAP_HANA_STUDIO\IMC_STUDIO2_212_2-80000323.SAR"
-    $jredest = "C:\Program Files\jdk-12_windows-x64_bin.exe"
-    $puttydest = "C:\SapBits\SAP_HANA_STUDIO\putty.exe"
+    $jredest = "C:\Program Files\jdk-12_windows-x64_bin.zip"
+    $puttydest = "C:\Users\testuser\Desktop\putty.exe"
     #7zdest = "C:\Program Files\7z.msi"
     $jrepath = "C:\Program Files"
     $hanapath = "C:\SapBits\SAP_HANA_STUDIO"
@@ -26,24 +26,18 @@ param (
     Invoke-WebRequest $sapcarUri -OutFile $sapcardest
     Invoke-WebRequest $hanastudioUri -OutFile $hanastudiodest
     Invoke-WebRequest $jreUri -OutFile $jredest
-    Invoke-WebRequest $7zUri -OutFile $7zdest
+    #Invoke-WebRequest $7zUri -OutFile $7zdest
     Invoke-WebRequest $puttyUri -OutFile $puttydest
     
-    write-host "installing 7zip and extracting JAVA"
-    cd $jrepath
-    .\7z.msi /quiet
-    cd "C:\Program Files\7-Zip\"
-    .\7z.exe e "C:\Program Files\serverjre-9.0.1_windows-x64_bin.tar.gz" "-oC:\Program Files"
-    .\7z.exe x -y "C:\Program Files\serverjre-9.0.1_windows-x64_bin.tar" "-oC:\Program Files"
-    
-    cd $hanapath
-    write-host "installing PuTTY"
-    .\putty-64bit-0.70-installer.msi /quiet
+    #write-host "installing 7zip and extracting JAVA"
+    write-host "Installing JAVA"
+    Expand-Archive -Path $jredest -DestinationPath $jrepath
+       
     
     write-host "extracting and installing HANA Studio"
     .\sapcar.exe -xfv IMC_STUDIO2_212_2-80000323.SAR
     
-    set PATH=%PATH%C:\Program Files\jdk-9.0.1\bin;
+    set PATH=%PATH%C:\Program Files\jdk-12\bin;
     set HDB_INSTALLER_TRACE_FILE=C:\Users\testuser\Documents\hdbinst.log
     cd C:\SAPbits\SAP_HANA_STUDIO\SAP_HANA_STUDIO\
-    .\hdbinst.exe -a C:\SAPbits\SAP_HANA_STUDIO\SAP_HANA_STUDIO\studio -b --path="C:\Program Files\sap\hdbstudio"
+    .\C:\SAPbits\SAP_HANA_STUDIO\SAP_HANA_STUDIO\hdbinst.exe -a C:\SAPbits\SAP_HANA_STUDIO\SAP_HANA_STUDIO\studio -b --path="C:\Program Files\sap\hdbstudio"
